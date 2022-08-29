cd9534116128ccfdf41122a3af1c92a135944d0d
Asher <jasherleslie@gmail.com>
2022-08-29 15:59:56 +1000

    Add space before spread operator.

--- src/react/src/Components/BrandColumns.js
+++ src/react/src/Components/BrandColumns.js
@@ -23,15 +23,15 @@ const BrandColumn = ({ children }) => {
     const ProjectColumn = setProjectColumn();
 
     const colSizes = isGWLandscape ? { lg: 4, md: 6 } : { lg: 8, md: 10 };
-    
+
     return (
         <Container className="h-100" fluid>
             <Row className="h-100">
                 <ProjectColumn />
                 <Col className="align-self-center" style={{ marginTop: '5rem' }}>
                     <Row className="justify-content-md-center">
-                        <Col {...colSizes }>
-                            { isGWLandscape && 
+                        <Col { ...colSizes }>
+                            { isGWLandscape &&
                                 <GWlandscapeLogo style={{ width: '100%' }} className="mb-4"/> }
                             { children }
                         </Col>
